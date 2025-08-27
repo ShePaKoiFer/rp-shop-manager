32f877118b37f2b0262dcfbb4fd46034f719f109
ShePaKoiFer <gabrielbichoff@gmail.com>
2025-08-27 18:50:36 +0200

    Ajout du .gitignore pour sécuriser les configs

--- app/config/.env.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'db_host' => '127.0.0.1',        // ou "localhost"
-    'db_name' => 'rp_shop_manager',  // le nom exact de ta base dans phpMyAdmin
-    'db_user' => 'root',             // XAMPP par défaut
-    'db_pass' => '',                 // vide sur XAMPP
-];
